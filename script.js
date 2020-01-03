df4efe3a917d4c84a3bf41e79bc5d815717577bc
Soubhagya <38796292+monetree@users.noreply.github.com>
2020-01-03 12:25:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementsByClassName("full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view")[0].childNodes[2].click()
+  document.getElementsByClassName
+  ("full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view")
+  [0].childNodes[2].click()
+
+  listofbuttons = []
+
+  for(let b of buttons){
+    listofbuttons.push(b.childNodes[2])
+  }
+
+  for(let l of listofbuttons){
+    l.click()
+  }
